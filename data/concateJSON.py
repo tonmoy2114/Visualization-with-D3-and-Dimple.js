934d3b6e6c8f36d7bc95cfd45b985289a5642034
tonmoy2114 <tonmoy2114@gmail.com>
2018-06-10 20:54:51 -0700

    Bug fix and data updated

---
+++ data/concateJSON.py
@@ -0,0 +1,43 @@
+import json
+import io
+
+hashMap = {}
+stressMap = {}
+valid_states_map = {}
+arr = []
+HR_limit = 84
+with io.open('Steps_D2.json', encoding="utf-8") as json_file:
+	data = json.load(json_file)
+	index = 0
+	for item in data:
+        	hashMap[item['time']] = item['value']
+		arr.append([])
+		arr[index].append(item['time'])
+		arr[index].append(item['value'])
+		valid_states_map[item['time']] = 1
+		index = index + 1
+threshold = 119
+slot_size = 25
+min_rest = 3
+for i in range(0,len(arr)):
+	if arr[i][1] == 0:
+		continue
+	step_count = min(arr[i][1],threshold)
+	next_invalid_slot = (step_count/slot_size) + min_rest
+	for j in range(0,next_invalid_slot+1):
+		if(i+j>=len(arr)):
+			break
+		valid_states_map[arr[i+j][0]] = 0
+		
+#print valid_states_map
+output = open("HR_Steps.csv","a")
+output.write("time,heartrate,steps,stress,day\n") 
+with io.open('HR_D2.json', encoding="utf-8") as json_file:
+	data = json.load(json_file)
+	for item in data:
+		stress_value = 0
+		if item['value'] > HR_limit:
+			stress_value = (item['value'] - HR_limit) * valid_states_map[item['time']]
+		output.write(item['time']+","+str(item['value'])+","+str(hashMap[item['time']])+","+str(stress_value)+",D2,\n")
+	output.close()
+        	
